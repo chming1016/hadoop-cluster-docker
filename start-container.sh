a497266bf6ee64d6526f462b2d06b6f00093fbff
MING <chming1016>
2016-12-21 07:10:04 +0800

    updated

---
+++ start-container.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# the default node number is 3
+N=${1:-4}
+
+# start hadoop master container
+echo "start hadoop-master container..."
+docker run -itd \
+                --net=hadoop \
+                -p 50070:50070 \
+                -p 8088:8088 \
+                --name hadoop-master \
+                --hostname hadoop-master \
+                test:test &> /dev/null
+
+# start hadoop slave container
+i=1
+while [ $i -lt $N ]
+do
+        echo "start hadoop-slave$i container..."
+        docker run -itd \
+                        --net=hadoop \
+                        --name hadoop-slave$i \
+                        --hostname hadoop-slave$i \
+                        test:test &> /dev/null
+        i=$(( $i + 1 ))
+done
+
+sleep 1
+docker exec -it hadoop-master sh -c 'service ssh start' > /dev/null
+docker exec -it hadoop-slave1 sh -c 'service ssh start' > /dev/null
+docker exec -it hadoop-slave2 sh -c 'service ssh start' > /dev/null
+docker exec -it hadoop-slave3 sh -c 'service ssh start' > /dev/null
